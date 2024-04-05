5e42f802ac64e30221fab045d3083a940d2ef13c
mriitian <132086670+mriitian@users.noreply.github.com>
2024-04-05 11:33:34 +0530

    Add files via upload

---
+++ src/components/DailyInfo.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+
+export default function DailyInfo() {
+  return (
+    <div>
+      <div className="dailyinfo">
+        <h4>The Physical Cycle</h4>
+        <p>
+          The physical aspect is at its peak, signifying enhanced energy,
+          vitality, and strength. This period boosts physical well-being,
+          increasing vigor, endurance, and sexual drive. However, there's a risk
+          of overexertion due to amplified physical forces.
+        </p>
+      </div>
+      <div className="dailyinfo">
+        <h4>The Emotional Cycle</h4>
+        <p>
+          The emotional aspect indicates intensified feelings and connections
+          with others during high emotional cycles. This fosters an outward
+          focus, enhancing relationships, support, and understanding. Openness
+          increases, making it ideal to strengthen bonds and engage in creative
+          pursuits. Optimism, love, and creativity flourish, but there's a
+          caution against emotional overreactions or impulsive decisions.
+        </p>
+      </div>
+      <div className="dailyinfo">
+        <h4>The Intellectual Cycle</h4>
+        <p>
+          During high intellectual cycles, cognitive abilities peak. Analysis,
+          planning, and focus are enhanced, making it an excellent time for
+          problem-solving, creative thinking, and embracing new ideas. This
+          phase encourages intellectual growth and exploration, although it may
+          also bring frustration if opportunities are scarce. Openness to new
+          challenges and ideas is heightened, paving the way for intellectual
+          achievements and advancements.
+        </p>
+      </div>
+      <div className="dailyinfo">
+        <h4>The Average Cycle</h4>
+        <p>
+          Days near cycle peaks are powerful, boosting endurance, emotional
+          vitality, and intellectual sharpness. These periods enhance personal
+          strengths, offering opportunities for peak performance in activities
+          aligned with each cycle's domain.
+        </p>
+      </div>
+    </div>
+  );
+}
